118fee4ae38a7f0d29f6dc8690d3c48280db1a53
Aru9518 <35200168+Aru9518@users.noreply.github.com>
2020-03-29 05:12:24 +0200

    Add files via upload

--- menu.py
+++ menu.py
@@ -20,16 +20,46 @@ print("1. Apagar Dispositivo \n"
      "3. Consultar Estado de los Dispositivos \n"
      "4. Resetear Dispositivo \n")
 
-analizador = argparse.ArgumentParser(description='Script para la gestión de los dispositios Mikrotik')
-analizador.parse_args()
+analizador = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description='Script para la gestión de los dispositios Mikrotik')
+analizador.add_argument('-x', help='Sale de la aplicación', default=False, action="store_true")
+#subanalizador = analizador.add_subparsers(help='comandos')
+analizador.add_argument('operacion', choices=['Apagar', 'Reiniciar', 'Consultar', 'Resetear'],  help="Permite escoger entre una de las 4 opciones descritas anteriormente")
 
-    #analizador.add_argument('-x', help='Sale de la aplicación', action= exit())
-op = 0;
-print("Introduzca opción:")
-op = input();
+group = analizador.add_mutually_exclusive_group(required=True)
+group.add_argument('-a', action="store_true", dest='todos', default=False, help="Todos los dispositivos")
+group.add_argument('-d', nargs='+', choices=range(1, 6), type=int, dest='dispositivo', help="Uno o más dispositivos, se requiere "
+                                                                        "de indicar mediante un entero del 1 al 5 el o los dispositivos a los cuales se quiere realizar la operación")
 
-print("La opción introducida es: " + op)
-if(op == '1'):
+
+analizador.add_argument('--version', action='version',
+                    version='%(prog)s 1.0')
+
+
+argumento = analizador.parse_args()
+
+print(argumento)
+todos = argumento.todos
+dispositivo = argumento.dispositivo
+'''if(analizador.parse_args()):
+    
+    print(argumento)
+
+else:
+    op = 0;
+    print("Introduzca opción:")
+    op = input();
+    '''
+
+#print(" argumento = " + argumento)
+
+#argumento = analizador.parse_args()
+#print("Se ha escogido:" + argumento.operacion)
+if(argumento.x):
+    print("Saliendo del programa...")
+    exit()
+
+#print("La opción introducida es: " + op)
+if(argumento.operacion == 'Apagar'):
     print(
         "Por favor seleccione uno o varios dispositivos que desee apagar, en caso de querer varias opciones deberá introducirlas seguidamente y sin espacios. "
         "Ejemplo para el reinicio de los dispositivos 1, 2 y 3 sería = 123.")
@@ -40,7 +70,7 @@ if(op == '1'):
           "3. Dispositivo Router1 \n"
           "4. Dispositivo Router2 \n"
           "5. Dispositivo Router3 \n")
-elif (op == '2'):
+elif (argumento.operacion == 'Reiniciar'):
     print(
         "Por favor seleccione uno o varios dispositivos que desee reiniciar, en caso de querer varias opciones deberá introducirlas seguidamente y sin espacios. "
         "Ejemplo para el reinicio de los dispositivos 1, 2 y 3 sería = 123.")
@@ -54,9 +84,9 @@ elif (op == '2'):
 
 
     #reboot()
-elif (op == '3'):
+elif (argumento.operacion == 'Consultar'):
     estado()
-elif (op == '4'):
+elif (argumento.operacion == 'Resetear'):
     print(
         "Por favor seleccione uno o varios dispositivos que desee resetear, en caso de querer varias opciones deberá introducirlas seguidamente y sin espacios. "
         "Ejemplo para el reinicio de los dispositivos 1, 2 y 3 sería = 123.")
@@ -68,9 +98,18 @@ elif (op == '4'):
           "4. Dispositivo Router2 \n"
           "5. Dispositivo Router3 \n")
 
+#analizador.add_argument("dispositivo", choices=['-a', '-s', '-w', '-r1' , '-r2', '-r3'], nargs='+', type=int, help="Elige entre la opción -a,  uno o varios dispositivos ")
+#argumento = analizador.parse_args()
+#print(argumento)
+#patron = re.compile('[0-5]')
+#dispositivo = input()
 
-patron = re.compile('[0-5]')
-dispositivo = input()
+
+
+
+if(argumento.x):
+    print("Saliendo del programa...")
+    exit()
 
 with open('dispositivos.json', 'r') as f: #Se realiza la lectura del fichero JSON
     direcciones_dict = json.load(f) #Se guardan los datos del fichero JSON en la variable direcciones_dict que nos permitirá acceder a los diversos campos del JSON
@@ -101,10 +140,12 @@ def connection(host, port, username, password):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.load_system_host_keys()
         # Connect to the host.
-        ssh.connect(host, port, username, password)
+
+        ssh.connect(host, port, username, password, timeout=4)
+
 
         print("Conexión realizada")
-        return (ssh)
+
         #tm = tiempo.tiempo()  # Calculamos el tiempo pasado en segundos desde el año 1970
     except AuthenticationException:
         print("Authentication failed, please verify your credentials: %s")
@@ -115,65 +156,78 @@ def connection(host, port, username, password):
     except BadHostKeyException as badHostKeyException:
         print("Unable to verify server's host key: %s" % badHostKeyException)
         sys.exit(1)
+    except Exception:
+        print("Ha ocurrido un error al intentar conectar con el dispositivo")
+        exit()
+
+    return (ssh)
 
 
-print("El patron del dispositivo es = ")
-print(patron.search(dispositivo))
+#print("El patron del dispositivo es = ")
+#print(patron.search(dispositivo))
 
-if (patron.findall(dispositivo, 0,4)): #Se comprueba que lo que el usuario ha enviado por teclado corresponde con la expresión regular
-    numero = int(dispositivo) #Se pasa el string a entero
-    if (dispositivo == "0"): #1er Caso si se selecciona 0 significa que la acción de Apagar, Reiniciar o Resetear corresponde a todos los dispositivos
-        if(op == "1"):
+#if (patron.findall(dispositivo, 0,4)): #Se comprueba que lo que el usuario ha enviado por teclado corresponde con la expresión regular
 
-            for direccion in direcciones_dict.get("devices"):
-                ip = direccion.get("nics")['management']['IP']
-                ssh = connection(ip, port, username, password)
-                apagar(ssh)
+    # argumento = analizador.parse_args()
+print(todos)
+#numero = int(dispositivo) #Se pasa el string a entero
 
-        elif(op=="2"):
-            for direccion in direcciones_dict.get("devices"):
-                ip = direccion.get("nics")['management']['IP']
-                ssh = connection(ip, port, username, password)
-                reboot(ssh)
-        elif(op=="3"):
-            for direccion in direcciones_dict.get("devices"):
-                ip = direccion.get("nics")['management']['IP']
-                ssh = connection(ip, port, username, password)
-                reset(ssh, ip, port, username, password)
+if (todos): #1er Caso si se selecciona 0 significa que la acción de Apagar, Reiniciar o Resetear corresponde a todos los dispositivos
+    print("He entrado en la opcion -a")
+    if(argumento.operacion == "Apagar"):
 
+        for direccion in direcciones_dict.get("devices"):
+            ip = direccion.get("nics")['management']['IP']
+            ssh = connection(ip, port, username, password)
+            apagar(ssh)
 
-    else: #En caso contrario corresponde al número de dispositivos que escribiese el usuario por teclado
-        cont = 0
-        if (op == "1"):
-
-            while (cont < len(dispositivo)): #Mientras el contador (que comienza en 0) sea < el nº de direcciones que hay en el JSON itera.
-                numero = int(dispositivo[cont])
-                ip = direcciones_dict.get("devices")[numero-1].get("nics")['management']['IP']#Se le resta 1 porque al acceder al JSON la primera posición es la 0
-                ssh = connection(ip, port, username, password) #Se llama a connection y se obtiene la conexión ssh para una dirección de dispositivo dada.
-                apagar(ssh) #Se llama a la función apagar que apagará el dispositivo enviándo el comando mediante dicha conexión ssh
-                cont += 1
-
-        elif (op == "2"):
-            while (cont < len(dispositivo)):
-                numero = int(dispositivo[cont])
-                print("El numero es = ")
-                print(numero)
-                ip = direcciones_dict.get("devices")[numero-1].get("nics")['management']['IP'] #Se le resta 1 porque al acceder al JSON la primera posición es la 0
-                print("La dirección del dispositivo a reiniciar es = " + ip)
-                ssh = connection(ip, port, username, password)
-                reboot(ssh)
-                cont += 1
-        elif (op == "3"):
-            while (cont < len(dispositivo)):
-                numero = int(dispositivo[cont])
-                ip = direcciones_dict.get("devices")[numero - 1].get("nics")['management']['IP']#Se le resta 1 porque al acceder al JSON la primera posición es la 0
-                ssh = connection(ip, port, username, password)
-                reset(ssh, ip, port, username, password)
-                cont += 1
+    elif(argumento.operacion=="Reiniciar"):
+        for direccion in direcciones_dict.get("devices"):
+            ip = direccion.get("nics")['management']['IP']
+            ssh = connection(ip, port, username, password)
+            reboot(ssh)
+    elif(argumento.operacion=="Resetear"):
+        for direccion in direcciones_dict.get("devices"):
+            ip = direccion.get("nics")['management']['IP']
+            ssh = connection(ip, port, username, password)
+            reset(ssh, ip, port, username, password)
 
 
+
+else: #En caso contrario corresponde al número de dispositivos que escribiese el usuario por teclado
+    cont = 0
+    if (argumento.operacion == "Apagar"):
+
+        while (cont < len(dispositivo)): #Mientras el contador (que comienza en 0) sea < el nº de direcciones que hay en el JSON itera.
+            numero = dispositivo[cont]
+            ip = direcciones_dict.get("devices")[numero-1].get("nics")['management']['IP']#Se le resta 1 porque al acceder al JSON la primera posición es la 0
+            ssh = connection(ip, port, username, password) #Se llama a connection y se obtiene la conexión ssh para una dirección de dispositivo dada.
+            apagar(ssh) #Se llama a la función apagar que apagará el dispositivo enviándo el comando mediante dicha conexión ssh
+            cont += 1
+
+    elif (argumento.operacion == "Reiniciar"):
+        while (cont < len(dispositivo)):
+            numero = int(dispositivo[cont])
+            print("El numero es = ")
+            print(numero)
+            ip = direcciones_dict.get("devices")[numero-1].get("nics")['management']['IP'] #Se le resta 1 porque al acceder al JSON la primera posición es la 0
+            print("La dirección del dispositivo a reiniciar es = " + ip)
+            ssh = connection(ip, port, username, password)
+            reboot(ssh)
+            cont += 1
+    elif (argumento.operacion == "3"):
+        while (cont < len(dispositivo)):
+            numero = int(dispositivo[cont])
+            ip = direcciones_dict.get("devices")[numero - 1].get("nics")['management']['IP']#Se le resta 1 porque al acceder al JSON la primera posición es la 0
+            ssh = connection(ip, port, username, password)
+            reset(ssh, ip, port, username, password)
+            cont += 1
+
+'''
 else: #En caso contrario imprime error
     print("Error, vuelva al menú y seleccione de nuevo la opción que quiere.")
+
+'''
 
 
    # print("Restaurando...")
