2ada300061e8060636392576a2f25171a6097d06
Roberto <roberto.magan@uca.es>
2020-03-31 20:32:01 +0200

    How to check existing keys in dicts

--- read_json.py
+++ read_json.py
@@ -5,4 +5,9 @@ with open("dispositivos_rmc.json","r") as f:
 
 for device in data['devices']:
    # Getting all management IPs of all devices
-   print("Device {0} --> management IP {1}".format(device['name'],device['nics']['management']['IP']))
+   
+   if "management" in device['nics'].keys():
+        print("Device {0} --> management IP {1}".format(device['name'],device['nics']['management']['IP']))
+   if "data" in device['nics'].keys():
+        print("Device {0} --> data IP {1}".format(device['name'],device['nics']['data']['IP']))
+ 
